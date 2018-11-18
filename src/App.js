8ac6e93d5f5980d2baaff40cb62232a34056353b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-11-18 20:38:08 +0100

    add workshop info to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import './App.css';
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
+        <h1>React state management workshop</h1>
         Edit <code>src/App.js</code> to see the changes.
       </div>
     );
